e8ae9de65f8745055258d7423f2ed22c5a6bca69
Claude477 <esa.korkiakoski@gmail.com>
2016-10-18 18:45:52 +0300

    Hpet
    Check computer clock speed with this file.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace hpet
+{
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            label1.Content = System.Diagnostics.Stopwatch.Frequency / System.Double.Parse("1000000") + "MHz";
+        }
+    }
+}
